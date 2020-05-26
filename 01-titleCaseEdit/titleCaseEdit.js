8813efe9404d41dc17195b13eca556758a5c77a8
ryan4summer <ryan4summer0527@gmail.com>
2020-05-26 13:34:43 -0400

    test-01 finished

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,12 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  for(var i = 0; i <= title.length; i++){
+  	if(i == 0)
+  		title = title.charAt(0).toUpperCase() + title.slice(1);
+  	else if(title.charAt(i) == " ")
+  		title = title.slice(0, i)+ " " + title.charAt(i + 1).toUpperCase() + title.slice(i + 2);
+  }
+  return title;
 }
 
 // Do not edit this line;
