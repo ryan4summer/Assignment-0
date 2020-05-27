235b63aba9f4183ec260a04f7de2e998ba5881fb
ryan4summer <ryan4summer0527@gmail.com>
2020-05-27 13:42:32 -0400

    test-07 finished

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,11 @@
 function countOfAllIndexMatchingNumbers(nums) {
   // Insert code here;
+  var result = 0;
+  for(var i = 0; i < nums.length; i++){
+  	if(nums[i] == i)
+  		result++;
+  }
+  return result;
 }
 
 // Do not edit this line;
