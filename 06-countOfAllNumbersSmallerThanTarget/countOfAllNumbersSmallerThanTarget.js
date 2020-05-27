ebf509e69ffd811e2785442c00c351b05f9aa7cd
ryan4summer <ryan4summer0527@gmail.com>
2020-05-27 13:36:56 -0400

    test-06 finished

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,10 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
+  var result = 0;
+  for(var i = 0; i < nums.length; i ++)
+  	if(nums[i] < target)
+  		result++;
+  return result;
 }
 
 // Do not edit this line;
