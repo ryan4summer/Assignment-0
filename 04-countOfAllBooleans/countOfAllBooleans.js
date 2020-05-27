2d43e97144422a5583cf009b05fee061542fb80e
ryan4summer <ryan4summer0527@gmail.com>
2020-05-27 12:39:46 -0400

    test-04 finished

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,11 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  var result = 0;
+  for(var i = 0; i <= arr.length; i++ ){
+  	if(arr[i] === true || arr[i] === false)
+  		result++;
+  }
+  return result;
 }
 
 // Do not edit this line;
