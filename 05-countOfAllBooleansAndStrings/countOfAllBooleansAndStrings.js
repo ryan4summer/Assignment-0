3472609bca00dbabce1835d05898fc549101f030
ryan4summer <ryan4summer0527@gmail.com>
2020-05-27 13:05:27 -0400

    test-5 finished

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,11 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
+  var result = 0;
+  for(var i = 0; i <= arr.length; i++){
+  	if(typeof(arr[i]) == "string" || typeof(arr[i]) == "boolean")
+  		result ++;
+  }
+  return result;
 }
 
 // Do not edit this line;
