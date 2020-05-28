1aca5196834a00ccaf572c1d417b1de8cbe17ec7
ryan4summer <ryan4summer0527@gmail.com>
2020-05-28 01:25:18 -0400

    test-8 finished

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,11 @@
 function sumOfAllEvenNumbers(nums) {
   // Insert code here;
+  var result = 0;
+  for(var i = 0; i <= nums.length; i++){
+  	if(nums[i] % 2 == 0)
+  		result++;
+  }
+  return result;
 }
 
 // Do not edit this line;
