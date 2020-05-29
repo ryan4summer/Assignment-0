dda5717c7f2ec0038d5b86d59e0e325fd72f42a7
ryan4summer <ryan4summer0527@gmail.com>
2020-05-29 02:40:48 -0400

    test-12 finished

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,18 @@
 function fizzBuzz(start, end) {
   // Insert code here;
+  var arr = new Array();
+  for(var i = start; i <= end; i++){
+  	if(i % 3 == 0 && i % 5 != 0)
+  		arr.push("Fizz");
+  	else if(i % 5 == 0 && i % 3 != 0)
+  		arr.push("Buzz");
+  	else if(i % 3 == 0 && i % 5 == 0)
+  		arr.push("FizzBuzz");
+  	else
+  		arr.push(i);
+  }
+  return arr;
+
 }
 
 // Do not edit this line;
