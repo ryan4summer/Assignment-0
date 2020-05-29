2ffd4a55ce288980cd3685141dcceb033adca1a3
ryan4summer <ryan4summer0527@gmail.com>
2020-05-29 02:11:35 -0400

    test-10 finished

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,17 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+  var count = 0;
+  for(var i = 0; i < nums.length; i++){
+  	if(start == end){
+      count ++;
+  		break;
+  	}
+  	if(nums[i] == start){
+  		count++;
+  	  start = nums[i + 1];
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;
