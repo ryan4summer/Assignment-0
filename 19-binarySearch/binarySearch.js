6a7c24afce1e6424815e99443188193a2bcec757
ryan4summer <ryan4summer0527@gmail.com>
2020-05-29 05:41:42 -0400

    test-19 finished

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,6 +5,23 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+    if(nums.length == 1)
+    	return nums[0] == target;
+    if(nums.length % 2 == 0){
+    	if(target == (nums[Math.floor(nums.length / 2)-1]))
+	 		return true;
+	    else if(target < (nums[Math.floor(nums.length / 2)-1]))
+	 		return this.binarySearch(nums.slice(0,(nums.length / 2)),target);
+	 	else 
+	 		return this.binarySearch(nums.slice((nums.length / 2 )),target);
+ 	}else{
+ 		if(target == (nums[Math.floor(nums.length / 2)]))
+	 		return true;
+ 		if(target < (nums[Math.floor(nums.length / 2)]))
+	 		return this.binarySearch(nums.slice(0,((nums.length / 2) )),target);
+	 	else
+	 		return this.binarySearch(nums.slice((nums.length / 2) ),target);
+ 	}
   }
 }
 
