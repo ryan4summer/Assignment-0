21a571c5015fcbfeeb0e8f87dcb0f7e025457b27
ryan4summer <ryan4summer0527@gmail.com>
2020-05-29 03:33:37 -0400

    test-14 finished

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,23 @@
 function frequencyCounter(word) {
   // Insert code here;
+  var result = new Object();
+  var x,temp = "";
+  for(var i = 0; i < word.length; i++){
+  	if(i == 0)
+  		result[word.charAt(i)] = 1;
+  	else{
+	  	for(x in result){
+	  		if(word.charAt(i) == x){
+	  			result[x] = result[x] + 1;
+	  			break;
+	  		}
+	  		else{
+	  			result[word.charAt(i)] = 1;
+	  		}
+	  	}
+	 }
+  }
+  return result;
 }
 
 // Do not edit this line;
