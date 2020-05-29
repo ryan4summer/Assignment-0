29a55f7a8723b37c5d734f9bee15c9082c653d7e
ryan4summer <ryan4summer0527@gmail.com>
2020-05-29 03:52:38 -0400

    test-16 finished

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,10 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+  var result = 1;
+  for(var i = 0; i < args.length; i++){
+  	result *= args[i];
+  }
+  return result;
 }
 
 // Do not edit this line;
