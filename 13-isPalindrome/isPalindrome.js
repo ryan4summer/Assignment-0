f055cb4ab67bd4e263158e21c5198d64d6f4497f
ryan4summer <ryan4summer0527@gmail.com>
2020-05-29 02:53:46 -0400

    test-13 finished

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,11 @@
 function isPalindrome(word) {
   // Insert code here;
+  var result = true;
+  for(var i = 0; i < (word.length/2); i++){
+  	if(word.charAt(i) != word.charAt(word.length - i - 1))
+  		result = false;
+  }
+  return result;
 }
 
 // Do not edit this line;
