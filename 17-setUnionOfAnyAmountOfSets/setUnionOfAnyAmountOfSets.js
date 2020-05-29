540ade802d6e088a9a18184022e5cfd5ae097e8e
ryan4summer <ryan4summer0527@gmail.com>
2020-05-29 04:12:37 -0400

    test-17 finished

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,13 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  let result = new Set();
+  for(var i = 0; i < args.length; i++){
+  	args[i].forEach(num =>{
+  		if(!result.has(num))
+  			result.add(num);
+  	});
+  }
+  return result;
 }
 
 // Do not edit this line;
