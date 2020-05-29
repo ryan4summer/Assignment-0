e2f54a12781b34f63dee41b02fc44d4eade235ea
ryan4summer <ryan4summer0527@gmail.com>
2020-05-29 04:32:31 -0400

    test-18 finished

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,21 @@
 function pairSum(nums, target) {
   // Insert code here;
+  var result = false;
+  var temp = 0;
+  if(nums.length <= 1 )
+  	throw 'The input of array should not less than 2';
+  for(var i = 0; i < nums.length; i++){
+  	for(var j = 0; j < nums.length; j++){
+  		if(i == j)
+  			break;
+  		result = ((nums[i] + nums[j])==target);
+  		if(result)
+  			return result;
+  	}
+  }
+
+
+  return result;
 }
 
 // Do not edit this line;
